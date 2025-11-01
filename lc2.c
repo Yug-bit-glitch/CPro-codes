80f21a7dd354d2cb11a42fd34677579547b03f50
Yug-bit-glitch <yugpatel2876@gmail.com>
2025-11-01 22:22:36 +0530

    git add

--- lc2.c
+++ lc2.c
@@ -10,6 +10,7 @@ int main(){
 // code wriitten as logic to implement the sum two numbers whose digits are two linked lists
 //creating a dummy head as we need to kind of reverse it
 node*dummy=malloc(sizeof(node));
+dummy->next=NULL;
 // curr points to dummy head
 node*curr=dummy;
 // we are implementing addition from lsb (initial carry assumed=0)
@@ -19,23 +20,26 @@ while(l1 != NULL || l2 != NULL || carry !=0){
     int sum=carry;
     if(l1 != NULL){
         sum += l1->val;
-        l1->next;
+        // l1->next is wrong
+     l1=l1->next;
     }
     if(l2 != NULL){
         sum += l2->val;
-        l2->next;
+        l2=l2->next;
     }
     carry=sum/10;
      
-    node* sum=malloc(sizeof(node));
-    sum->val=sum % 10;
-
-    curr->next=sum;
+    node* Node=malloc(sizeof(node));
+    Node->val=sum % 10;
+    // bug was here forgot to point to NULL
+    Node->next=NULL;
+    curr->next=Node;
     curr=curr->next;
 
 }
 node*result=dummy->next;
 free(dummy);
+
 return result;
     return 0;
 }
