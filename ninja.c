adeca98e949d287d8d02e9fded0cbb2aa6da59c9
Yug-bit-glitch <yugpatel2876@gmail.com>
2025-11-02 14:26:50 +0530

    git add

---
+++ ninja.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    
+    if(n == 0) {
+        printf("0");
+        return 0;
+    }
+    if(n == 1) {
+        printf("1");
+        return 0;
+    }
+    if(n == 2) {
+        printf("2");
+        return 0;
+    }
+    if(n == 3) {
+        printf("4");
+        return 0;
+    }
+    
+    // Memoization didnt work trying malloc
+    int *arr=(int*)malloc(n*sizeof(int));
+    // Fill array from index 4 to n using recurrence relation
+    // arr[i] = arr[i-1] + arr[i-2] + arr[i-3]
+    for(int i = 4; i <= n; i++) {
+        arr[i] = arr[i-1] + arr[i-2] + arr[i-3];
+    }
+    
+    printf("%d", arr[n]);
+    return 0;
+}
